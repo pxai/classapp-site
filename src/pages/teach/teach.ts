e134747b44d0c3be644fd240faceb88acfcb7457
pxai <pello_altadill@cuatrovientos.org>
2017-02-26 00:50:02 +0100

    Added teach component

---
+++ src/pages/teach/teach.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-teach',
+  templateUrl: 'teach.html'
+})
+export class TeachPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
