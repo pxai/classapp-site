4577e4c1b8d2c80c31a8064cc20e2e69b2e6e265
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-11-07 23:54:50 +0100

    Trying wioth App.test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import detectBrowserLanguage from 'detect-browser-language'
+import './App.css';
+import { I18n, Trans } from 'react-i18next';
+import Demo from './components/Demo/Demo';
+import Header from './components/Header/Header';
+import SubHeader from './components/SubHeader/SubHeader'
+import AppBar from 'material-ui/AppBar';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
